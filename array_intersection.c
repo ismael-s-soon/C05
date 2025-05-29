690f7c3f6cae02fdfaf31d016475641b246e7a83
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-29 16:26:27 +0100

    Update array_intersection.c

--- array_intersection.c
+++ array_intersection.c
@@ -1,29 +1,46 @@
 #include <stdio.h>
 
 int main() {
-    int n, i, val, count = 0;
-    int tab[100];
+    int n1, n2, i, j, k = 0;
+    int tab1[100], tab2[100], intersection[100];
 
-    // Lire la taille du tableau
-    scanf("%d", &n);
-
-    // Lire les éléments du tableau
-    for(i = 0; i < n; i++) {
-        scanf("%d", &tab[i]);
+    // Lecture de la taille et des éléments du premier tableau
+    scanf("%d", &n1);
+    for(i = 0; i < n1; i++) {
+        scanf("%d", &tab1[i]);
     }
 
-    // Lire l'élément à rechercher
-    scanf("%d", &val);
+    // Lecture de la taille et des éléments du deuxième tableau
+    scanf("%d", &n2);
+    for(i = 0; i < n2; i++) {
+        scanf("%d", &tab2[i]);
+    }
 
-    // Compter combien de fois val apparaît dans le tableau
-    for(i = 0; i < n; i++) {
-        if(tab[i] == val) {
-            count++;
+    // Calcul de l'intersection
+    for(i = 0; i < n1; i++) {
+        for(j = 0; j < n2; j++) {
+            if(tab1[i] == tab2[j]) {
+                // Vérifier si l'élément est déjà présent dans l'intersection
+                int deja_present = 0;
+                for(int m = 0; m < k; m++) {
+                    if(intersection[m] == tab1[i]) {
+                        deja_present = 1;
+                        break;
+                    }
+                }
+                if(!deja_present) {
+                    intersection[k++] = tab1[i];
+                }
+            }
         }
     }
 
-    // Afficher le résultat
-    printf("L'élément %d apparaît %d fois.\n", val, count);
+    // Affichage de l'intersection
+    printf("Intersection : ");
+    for(i = 0; i < k; i++) {
+        printf("%d ", intersection[i]);
+    }
+    printf("\n");
 
     return 0;
 }
