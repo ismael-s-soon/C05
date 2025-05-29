557479172dd9fb662640fdd841d73b44ec7cc0b1
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-29 19:41:39 +0100

    Update array_intersection.c

--- array_intersection.c
+++ array_intersection.c
@@ -2,33 +2,39 @@
 
 int main() {
     int n1, n2, i, j, k = 0;
-    int t1[100], t2[100], res[100];
+    int a[100], b[100], c[100];
 
     scanf("%d", &n1);
-    for (i = 0; i < n1; i++) scanf("%d", &t1[i]);
+    for(i = 0; i < n1; i++) {
+        scanf("%d", &a[i]);
+    }
 
     scanf("%d", &n2);
-    for (i = 0; i < n2; i++) scanf("%d", &t2[i]);
+    for(i = 0; i < n2; i++) {
+        scanf("%d", &b[i]);
+    }
 
-    for (i = 0; i < n1; i++) {
-        for (j = 0; j < n2; j++) {
-            if (t1[i] == t2[j]) {
-                int dup = 0;
-                for (int m = 0; m < k; m++) {
-                    if (res[m] == t1[i]) {
-                        dup = 1;
+    for(i = 0; i < n1; i++) {
+        for(j = 0; j < n2; j++) {
+            if(a[i] == b[j]) {
+                int exists = 0;
+                for(int x = 0; x < k; x++) {
+                    if(c[x] == a[i]) {
+                        exists = 1;
                         break;
                     }
                 }
-                if (!dup) res[k++] = t1[i];
+                if(!exists) {
+                    c[k++] = a[i];
+                }
                 break;
             }
         }
     }
 
     printf("Intersection :");
-    for (i = 0; i < k; i++) {
-        printf(" %d", res[i]);
+    for(i = 0; i < k; i++) {
+        printf(" %d", c[i]);
     }
     printf("\n");
 
