5e9b094f764a4416f3f4cc090328e27f2489554c
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-29 19:36:01 +0100

    Update array_intersection.c

--- array_intersection.c
+++ array_intersection.c
@@ -1,55 +1,34 @@
 #include <stdio.h>
 
-void sort(int arr[], int n) {
-    for (int i = 0; i < n-1; i++) {
-        for (int j = i+1; j < n; j++) {
-            if (arr[i] > arr[j]) {
-                int tmp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = tmp;
-            }
-        }
-    }
-}
-
 int main() {
     int n1, n2, i, j, k = 0;
-    int tab1[100], tab2[100], intersection[100];
+    int t1[100], t2[100], res[100];
 
     scanf("%d", &n1);
-    for(i = 0; i < n1; i++) {
-        scanf("%d", &tab1[i]);
-    }
+    for (i = 0; i < n1; i++) scanf("%d", &t1[i]);
 
     scanf("%d", &n2);
-    for(i = 0; i < n2; i++) {
-        scanf("%d", &tab2[i]);
-    }
+    for (i = 0; i < n2; i++) scanf("%d", &t2[i]);
 
-    for(i = 0; i < n1; i++) {
-        for(j = 0; j < n2; j++) {
-            if(tab1[i] == tab2[j]) {
-                int already_in = 0;
-                for(int m = 0; m < k; m++) {
-                    if(intersection[m] == tab1[i]) {
-                        already_in = 1;
+    for (i = 0; i < n1; i++) {
+        for (j = 0; j < n2; j++) {
+            if (t1[i] == t2[j]) {
+                int dup = 0;
+                for (int m = 0; m < k; m++) {
+                    if (res[m] == t1[i]) {
+                        dup = 1;
                         break;
                     }
                 }
-                if(!already_in) {
-                    intersection[k++] = tab1[i];
-                }
+                if (!dup) res[k++] = t1[i];
+                break;
             }
         }
     }
 
-    // Trier le résultat
-    sort(intersection, k);
-
-    // Affichage
     printf("Intersection :");
-    for(i = 0; i < k; i++) {
-        printf(" %d", intersection[i]);
+    for (i = 0; i < k; i++) {
+        printf(" %d", res[i]);
     }
     printf("\n");
 
