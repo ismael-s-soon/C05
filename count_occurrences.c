340b44320449e318a12f790644866312298d9d78
ismael-s-soon <docteurpapa10@gmail.com>
2025-06-01 22:28:33 +0100

    Update count_occurrences.c

--- count_occurrences.c
+++ count_occurrences.c
@@ -1,21 +1,45 @@
 #include <stdio.h>
 
 int main() {
-    int n, i, element, compteur = 0, tableau[100];
+    int n1, n2, i, j, k = 0;
+    int tab1[100], tab2[100], intersection[100];
 
-    // Lecture du nombre d’éléments et des éléments du tableau
-    scanf("%d", &n);
-    for(i = 0; i < n; i++) scanf("%d", &tableau[i]);
+    scanf("%d", &n1);  // Lecture de la taille du premier tableau
+    for(i = 0; i < n1; i++) {
+        scanf("%d", &tab1[i]);  // Lecture des éléments du premier tableau
+    }
 
-    // Lecture de l’élément à rechercher
-    scanf("%d", &element);
+    scanf("%d", &n2);  // Lecture de la taille du deuxième tableau
+    for(i = 0; i < n2; i++) {
+        scanf("%d", &tab2[i]);  // Lecture des éléments du deuxième tableau
+    }
 
-    // Comptage du nombre d’occurrences de l’élément
-    for(i = 0; i < n; i++)
-        if(tableau[i] == element) compteur++;
+    // Comparaison croisée entre les deux tableaux
+    for(i = 0; i < n1; i++) {
+        for(j = 0; j < n2; j++) {
+            if(tab1[i] == tab2[j]) {
+                // Vérifier si l'élément est déjà dans l'intersection (pour éviter les doublons)
+                int deja_present = 0;
+                for(int m = 0; m < k; m++) {
+                    if(intersection[m] == tab1[i]) {
+                        deja_present = 1;
+                        break;
+                    }
+                }
+                // Si l'élément n'est pas encore ajouté, on l'ajoute
+                if(!deja_present) {
+                    intersection[k++] = tab1[i];
+                }
+            }
+        }
+    }
 
     // Affichage du résultat
-    printf("L'élément %d apparaît %d fois.\n", element, compteur);
+    printf("Intersection : ");
+    for(i = 0; i < k; i++) {
+        printf("%d ", intersection[i]);
+    }
+    printf("\n");
 
     return 0;
 }
