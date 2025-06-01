a95ef87f42c8ac1fcc1f55ef13832166ebbdc63b
ismael-s-soon <docteurpapa10@gmail.com>
2025-06-01 22:55:33 +0100

    Update min_in_array.c

--- min_in_array.c
+++ min_in_array.c
@@ -3,23 +3,26 @@
 int main() {
     int n, i, min, tab[100];
 
-    // Lire le nombre d'éléments à entrer
+    // Lire le nombre d'éléments à stocker dans le tableau
     scanf("%d", &n);
 
-    // Lire les éléments du tableau
-    for(i = 0; i < n; i++)
+    // Remplir le tableau avec les éléments saisis par l'utilisateur
+    for(i = 0; i < n; i++) {
         scanf("%d", &tab[i]);
+    }
 
-    // Initialiser le minimum avec le premier élément
+    // Assigner la première valeur du tableau comme valeur minimale initiale
     min = tab[0];
 
-    // Chercher le plus petit élément
-    for(i = 1; i < n; i++)
-        if(tab[i] < min)
-            min = tab[i];
+    // Parcourir le reste du tableau pour identifier la plus petite valeur
+    for(i = 1; i < n; i++) {
+        if(tab[i] < min) {
+            min = tab[i]; // Mettre à jour la valeur minimale si une valeur plus petite est trouvée
+        }
+    }
 
-    // Afficher le minimum trouvé
-    printf("%d\n", min);
+    // Afficher la plus petite valeur trouvée dans le tableau
+    printf("Le minimum est : %d\n", min);
 
     return 0;
 }
