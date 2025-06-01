2f292ec3e8f900310e7731bd767308a1dd063dcb
ismael-s-soon <docteurpapa10@gmail.com>
2025-06-01 22:48:23 +0100

    Update merge_arrays.c

--- merge_arrays.c
+++ merge_arrays.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
 
 int main() {
-    int n1, n2, i, t1[100], t2[100], fusion[200];
+    int n1, n2, i;
+    int t1[100], t2[100], fusion[200];  // Déclaration des tableaux et du tableau de fusion
 
-    // Lecture de la taille et des éléments du premier tableau
-    scanf("%d", &n1);
-    for(i = 0; i < n1; i++) 
-        scanf("%d", &t1[i]);
+    scanf("%d", &n1);  // Lecture de la taille du premier tableau
+    for(i = 0; i < n1; i++) {
+        scanf("%d", &t1[i]);  // Lecture des éléments du premier tableau t1
+    }
 
-    // Lecture de la taille et des éléments du deuxième tableau
-    scanf("%d", &n2);
-    for(i = 0; i < n2; i++) 
-        scanf("%d", &t2[i]);
+    scanf("%d", &n2);  // Lecture de la taille du deuxième tableau
+    for(i = 0; i < n2; i++) {
+        scanf("%d", &t2[i]);  // Lecture des éléments du deuxième tableau t2
+    }
 
-    // Copie des éléments de t1 dans fusion
-    for(i = 0; i < n1; i++) 
+    // Copie des éléments du premier tableau dans le tableau fusion
+    for(i = 0; i < n1; i++) {
         fusion[i] = t1[i];
+    }
 
-    // Copie des éléments de t2 à la suite dans fusion
-    for(i = 0; i < n2; i++) 
+    // Copie des éléments du deuxième tableau à la suite dans fusion
+    for(i = 0; i < n2; i++) {
         fusion[n1 + i] = t2[i];
+    }
 
     // Affichage du tableau fusionné
-    for(i = 0; i < n1 + n2; i++) 
+    printf("Tableau fusionné : ");
+    for(i = 0; i < n1 + n2; i++) {
         printf("%d ", fusion[i]);
+    }
 
     return 0;
 }
